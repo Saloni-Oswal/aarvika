6b29dee2d42e96e957af0ba02ce87ae752cd8165
Saloni Oswal <salonioswal22@gmail.com>
2025-10-06 10:33:09 -0400

    Download fonts via vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteWebfontDownload([
+      "https://fonts.googleapis.com/css2?family=Century+Gothic:wght@400;700&display=swap",
+    ]),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "src"),
